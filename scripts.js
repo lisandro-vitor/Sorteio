60ff38b62bc3785062884cc8d67f4322538f2e11
lisandro-vitor <lisandrovitor@hotmail.com>
2024-02-08 21:12:23 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function generateNumber() {
-const min = Math.ceil(document.querySelector(".input-min").value)
-const max = Math.floor(document.querySelector(".input-max").value)
-const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
-alert(result)
+    if (min >= max) {
+        alert("O valor minimo tem que ser Menor que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
 }
+
+
